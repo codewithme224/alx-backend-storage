d73c266395637cc00fd1d15fa21ced26d5734a84
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-12 12:15:39 +0000

    Task added

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,14 @@
 -- Comment: MySQL trigger that validates email
 
 
-
+DELIMITER $$
 CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$' THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
-    ELSE
-        SET NEW.valid_email = 1;
     END IF;
-END
+END $$
+
+DELIMITER;
