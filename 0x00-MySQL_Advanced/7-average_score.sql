d561b874b76ee45e8ad20013e4d7a75fb53a42ab
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-12 12:23:52 +0000

    Task added

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,10 @@
 -- Script to create the procedure ComputeAverageScoreForUser in your MySQL database
 
-
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    INSERT INTO average_score (user_id, score)
-    VALUES (user_id, (SELECT AVG(score) FROM corrections WHERE user_id = user_id));
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+    WHERE id = user_id;
 END
