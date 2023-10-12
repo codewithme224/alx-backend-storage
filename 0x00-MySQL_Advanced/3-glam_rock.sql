f7cb31c67eb483f52ff30a4ce127227f3c26dc03
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-12 12:07:23 +0000

    Task added

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Script that ranks glam rock bands by their lifespan
 
 
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
