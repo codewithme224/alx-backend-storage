4e71bdce99a9576f510da8cd60b9a0a3ee0143e0
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-12 12:12:07 +0000

    Task added

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,5 +4,4 @@
 CREATE TRIGGER decrement AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items
-SET quantity = quantity - 1
-WHERE id = NEW.item_id;
+SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
