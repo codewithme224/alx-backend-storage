30899a683a9482eb72368126e8a88f27216239f8
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-17 19:45:06 +0000

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+    if mongo_collection:
+        return mongo_collection.find()
+    else:
+        return []
